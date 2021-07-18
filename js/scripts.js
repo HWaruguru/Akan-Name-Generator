7dbc551a3e3e159cdedcda93254fbb5bff312b58
Waruguru <hannah.waruguru8@gmail.com>
2021-07-19 00:42:35 +0300

    Add the event listeners

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ function getName(gender, day){
         1: "Adwoa",
         2: "Abenaa",
         3: "Akua",
-        4:  "Yaa",
+        4: "Yaa",
         5: "Afua",
         6: "Ama"
     };
@@ -28,4 +28,27 @@ function getName(gender, day){
     } else {
         return male[day]
     };
-}
+};
+
+function getGender(){
+    var gender = null;
+    if (document.getElementById("male").checked) {
+        gender = "male"
+    } else {
+        gender = "female"
+
+        };
+    return gender;
+};
+
+
+var form = document.getElementsByTagName("form")[0];
+form.addEventListener("submit", function(event){
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var day = document.getElementById("day").value;
+    var gender = getGender()
+    var dayOfWeek = getDayofWeek(year, month, day);
+    var name = getName(gender, dayOfWeek)
+    alert("Your Akan name is " + name)
+})
