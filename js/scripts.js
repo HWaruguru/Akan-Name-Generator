c6325179317f9a19dcc4975719929d7c7709ba25
Waruguru <hannah.waruguru8@gmail.com>
2021-07-19 00:02:08 +0300

    Add an if condition to get the day given gender and year of birth

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,30 @@ function getDayofWeek(year, month, day) {
     var date = new Date(year+"/"+month+"/"+day);
     return date.getDay();
 }
+
+function getName(gender, day){
+    var female = {
+        0: "Akosua",
+        1: "Adwoa",
+        2: "Abenaa",
+        3: "Akua",
+        4:  "Yaa",
+        5: "Afua",
+        6: "Ama"
+    };
+    var male = {
+        0: "Kwasi",
+        1: "Kwadwo",
+        2: "Kwabena",
+        3: "Kwaku",
+        4:  "Yaw",
+        5: "Kofi",
+        6: "Kwame"
+    };
+
+    if(gender === "female") {
+        return female[day]
+    } else {
+        return male[day]
+    };
+}
